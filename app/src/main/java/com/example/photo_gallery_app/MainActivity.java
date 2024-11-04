add86dc3240dfe4fc280079a2583251fd59feae2
luricisme <trinhnguyenluong182306@gmail.com>
2024-11-04 15:28:18 +0700

    Initial Design

---
+++ app/src/main/java/com/example/photo_gallery_app/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.photo_gallery_app;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.photo_gallery_app.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.green));
+//        }
+
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Mặc định ban đầu là HomeFragment
+        replaceFragment(new HomeFragment(), "Home");
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+//            switch(item.getItemId()){
+//                case R.id.home:
+//                    replaceFragment(new HomeFragment());
+//                    break;
+//
+//                case R.id.album:
+//                    replaceFragment(new AlbumFragment());
+//                    break;
+//
+//                case R.id.favorite:
+//                    replaceFragment(new FavoriteFragment());
+//                    break;
+//
+//                case R.id.more:
+//                    replaceFragment(new MoreFragment());
+//                    break;
+//            }
+            int itemId = item.getItemId();
+            if(itemId == R.id.home){
+                replaceFragment(new HomeFragment(), "Home");
+            }
+            else if(itemId == R.id.album)
+            {
+                replaceFragment(new AlbumFragment(), "Album");
+            }
+            else if(itemId == R.id.favorite){
+                replaceFragment(new FavoriteFragment(), "Favorite");
+            }
+            else if(itemId == R.id.more){
+                replaceFragment(new MoreFragment(), "More");
+            }
+
+            return true;
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu); // Inflate menu từ toolbar_menu.xml
+        return true;
+    }
+
+    private void replaceFragment(Fragment fragment, String title){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+        getSupportActionBar().setTitle(title);
+    }
+}
