bd22afc525fd0aa0a93136596ee0f6e50cd4d45e
22120197 <nguyenvinhluong242004@gmail.com>
2024-11-12 22:46:05 +0700

    Get image from device - home
    get and display in the home_fragment

--- app/src/main/java/com/example/photo_gallery_app/HomeFragment.java
+++ app/src/main/java/com/example/photo_gallery_app/HomeFragment.java
@@ -3,6 +3,8 @@ package com.example.photo_gallery_app;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,10 +12,23 @@ import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
 
+    public RecyclerView recyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Sử dụng view để truy cập RecyclerView
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        // Gọi hàm trong MainActivity
+        MainActivity mainActivity = (MainActivity) getActivity();
+        if (mainActivity != null) {
+            mainActivity.Load();
+        }
+
+
+        return view;
     }
 }
